ecbfda62ae7bc64631745ba9da3568f307941933
jg <jgmills1998@gmail.com>
2019-04-11 22:22:56 -0400

    added search artist button functionality

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse,JsonResponse,QueryDict
 from .forms import artistForm,albumForm,songForm
-from .models import Artist
+from .models import Artist,Album,Song
+from django.db.models.query import QuerySet
 
 from django.db.models import Q
 
@@ -18,29 +19,123 @@ def test(request):
 
 def result(request):
     if request.method == "GET":
-        form = request.GET
+        form = request.GET #get form from GET request
         form = form.copy()
-        form.pop('csrfmiddlewaretoken')
+        form.pop('csrfmiddlewaretoken') #remove token from request
+        # print(form)
+        # form.pop('artistSearch')
+        
+        artistQuery,albumQuery,songQuery = formatQuery(request,form)#create 3 sub queries
 
-        for value in form.copy().items():
-            if value[1] == "":
-                form.pop(value[0])
+        # artists,albums,songs = QuerySet()
+        print(artistQuery)
+        print(albumQuery)
+        print(songQuery)
+        
+        if form.__contains__('all'):
+            print(form.__contains__('all'))
+        elif form.__contains__('artistSearch'):
+            if(bool(artistQuery)):
+                artists = Artist.objects.filter(**artistQuery.dict())
+            else:
+                artists = Artist.objects.none()
 
-        query = Artist.objects.all()
+            if(bool(albumQuery)):
+                albums = Album.objects.filter(**albumQuery.dict())
+                if(bool(artists)):
+                    artists = artists.filter(artist_id__in=albums.values_list('artist_id')).select_related()
+                else:
+                    artists = Artist.objects.filter(artist_id__in=albums.values_list('artist_id')).select_related()
+            else:
+                albums = Album.objects.none()
 
-        # for key,value in form.items():
-        #     query.filter(key=value)
+            if bool(songQuery):
+                songs = Song.objects.filter(**songQuery.dict())
+
+                if(bool(artists)):
+                    artists = artists.filter(artist_id__in=songs.values_list('artist_id')).select_related()
+                else:
+                    artists = Artist.objects.filter(artist_id__in=songs.values_list('artist_id')).select_related()
 
 
-        query = Artist.objects.filter(name="Lil Wayne")
-        # query = Artist.objects.filter(name__iexact=form['name'])
+            else:
+                songs = Song.objects.none()
+            
+            # print(artistQuery)
+            # print(albumQuery)
+            # print(songQuery)
+            print(artists)
+            print(albums)
+            print(songs)
 
-        result = [entry for entry in query.values()]
-        # print(query.values())
-        # print(type(lis))
+        # print(bool(artistQuery))
+        # if not bool(artistQuery):
+        #     artists = Artist.objects.filter(**artistQuery.dict())
+
+# Album.objects.filter(artist__in=a.values_list('artist_id')).select_related()
+
+        query = Artist.objects.filter(**artistQuery.dict())
+        
+        result = [entry for entry in artists.values()]
+
         queryRes = {
-            'result':result
+            'artistResult':[entry for entry in artists.values()],
+            'albumResult':[entry for entry in albums.values()],
+            'songResult':[entry for entry in songs.values()]
         }
+        # print(queryRes)
+
+
 
         # print(Artist.objects.filter(name='lil Wayne')[0].DOB)
     return render(request,'search/result.html',queryRes)
+
+def formatQuery(request,form):
+
+    artistQuery = QueryDict().copy()
+    albumQuery = QueryDict().copy()
+    songQuery = QueryDict().copy()
+    
+    #format artist query
+    if form.get('name') != "":
+        artistQuery.appendlist('name',form.get('name'))
+    if form.get('DOB') != "":
+        artistQuery.appendlist('DOB',form.get('DOB'))
+    if form.get('Genre') != "":
+        artistQuery.appendlist('Genre',form.get('Genre'))
+    if form.get('monthly_views') != "":
+        artistQuery.appendlist('monthly_views',form.get('monthly_views'))
+    
+    #format album query
+
+    if form.get('title') != "":
+        albumQuery.appendlist('title',form.get('title'))
+    if form.getlist('artist')[0] != "":
+        albumQuery.appendlist('artist',form.getlist('artist')[0])
+    if form.get('label') != "":
+        albumQuery.appendlist('label',form.get('label'))        
+    if form.get('sales') != "":
+        albumQuery.appendlist('sales',form.get('sales'))
+    if form.get('year') != "":
+        albumQuery.appendlist('year',form.get('year'))
+
+
+    #formatsong query
+    if form.get('track') != "":
+        songQuery.appendlist('track',form.get('track'))
+    if form.get('album') != "":
+        songQuery.appendlist('album',form.get('album'))
+    if form.getlist('artist')[1] != "":
+        albumQuery.appendlist('artist',form.getlist('artist')[1])
+    if form.get('length') != "":
+        songQuery.appendlist('length',form.get('length'))        
+    if form.get('streams') != "":
+        songQuery.appendlist('streams',form.get('streams'))
+    if form.get('featured') != "":
+        songQuery.appendlist('featured',form.get('featured'))
+
+    # print(artistQuery.__contains__('name'))
+
+    return artistQuery,albumQuery,songQuery
+
+        
